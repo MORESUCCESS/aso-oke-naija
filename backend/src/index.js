ad8582e9f39de654b0c321c8d3f4da995f250160
ms2dwrld <hammedomoniyi3@gmail.com>
2026-08-13 02:30:17 +0100

    still fixing cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,8 +32,8 @@ app.use(helmet());
 console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL,
-    'http://localhost:5173',
+    process.env.FRONTEND_URL ||
+    'http://localhost:5173' ||
     'http://localhost:3000',
   ],
   credentials: true,
